dca98aa91bcd848ea8ede40d02caf399a4b0ca73
sidik181 <sidik.komarudiansah@gmail.com>
2021-09-18 23:09:43 +0700

    edit typo

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -79,7 +79,7 @@ class UsersService {
 		return id;
 	}
 
-	async getUsersbyUsername(username) {
+	async getUsersByUsername(username) {
 		const query = {
 			text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
 			values: [`%${username}%`],
